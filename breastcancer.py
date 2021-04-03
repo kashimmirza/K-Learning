19f4636d29255e74319d70b83540fbf0aa9234f7
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2021-04-03 13:19:20 +0600

    updated with pca, knn modeling

--- breastcancer.py
+++ breastcancer.py
@@ -101,3 +101,144 @@ plt.xticks(rotation = 90)
 plt.show()
 sns.pairplot(X_train_df[corr_features], diag_kind = "kde", markers = "+",hue = "target")
 plt.show()
+knn = KNeighborsClassifier(n_neighbors = 2)
+knn.fit(X_train, Y_train)
+y_pred = knn.predict(X_test)
+cm = confusion_matrix(Y_test, y_pred)
+acc = accuracy_score(Y_test, y_pred)
+score = knn.score(X_test, Y_test)
+
+print("Basic KNN Accuracy: % {}".format(acc))
+print("Score : ", score)
+print("CM : ", cm)
+def KNN_Best_Params(x_train, x_test, y_train, y_test):
+    
+    k_range = list(range(1,31))
+    weight_options = ["uniform","distance"]
+    print()
+    param_grid = dict(n_neighbors = k_range, weights = weight_options)
+     
+    knn = KNeighborsClassifier()
+    
+    grid = GridSearchCV(knn, param_grid, cv = 10, scoring = "accuracy")
+    grid.fit(x_train, y_train)
+    
+    print("Best training score : {} with paremeters : {}".format(grid.best_score_, grid.best_params_))
+    print()
+    
+    knn = KNeighborsClassifier(**grid.best_params_) # best paremetre olarak gelen değerlerimiz.
+    knn.fit(x_train, y_train)
+    
+    y_pred_test = knn.predict(x_test)
+    y_pred_train = knn.predict(x_train)
+    
+    cm_test = confusion_matrix(y_test, y_pred_test)
+    cm_train = confusion_matrix(y_train, y_pred_train)
+    
+    acc_test = accuracy_score(y_test, y_pred_test) 
+    acc_train = accuracy_score(y_train, y_pred_train)
+    print("Test Score: {}, Train Score: {}".format(acc_test, acc_train))
+    print()
+    print("CM Test: ",cm_test)
+    print("CM Train: ",cm_train)
+    
+    return grid
+#modeling with pca
+
+scaler = StandardScaler()
+x_scaled = scaler.fit_transform(x)
+pca = PCA(n_components = 2)
+pca.fit(x_scaled)
+X_reduced_pca = pca.transform(x_scaled)
+pca_data = pd.DataFrame(X_reduced_pca, columns = ["p1","p2"])
+pca_data["target"] = y
+
+plt.figure(figsize=(10,7))
+sns.scatterplot(x = "p1", y = "p2", hue = "target", data = pca_data)
+plt.title("PCA : p1 vs p2")
+X_train_pca, X_test_pca, Y_train_pca, Y_test_pca = train_test_split(X_reduced_pca, y, test_size = test_size, random_state = 42)
+
+grid_pca = KNN_Best_Params(X_train_pca, X_test_pca, Y_train_pca, Y_test_pca)
+cmap_light = ListedColormap(['orange',  'cornflowerblue'])
+cmap_bold = ListedColormap(['darkorange', 'darkblue'])
+h = .05 # step size in the mesh
+X = X_reduced_pca
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                     np.arange(y_min, y_max, h))
+
+Z = grid_pca.predict(np.c_[xx.ravel(), yy.ravel()])
+
+Z = Z.reshape(xx.shape)
+plt.figure(figsize=(10,7))
+plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+# Plot also the training points
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+            edgecolor='k', s=20)
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.title("%i-Class classification (k = %i, weights = '%s')"
+          % (len(np.unique(y)),grid_pca.best_estimator_.n_neighbors, grid_pca.best_estimator_.weights))
+
+
+
+#modeling with neighboring compornent
+nca = NeighborhoodComponentsAnalysis(n_components = 2, random_state = 42)
+nca.fit(x_scaled, y)
+X_reduced_nca = nca.transform(x_scaled)
+nca_data = pd.DataFrame(X_reduced_nca, columns = ["p1","p2"])
+nca_data["target"] = y
+plt.figure(figsize=(10,7))
+sns.scatterplot(x = "p1",  y = "p2", hue = "target", data = nca_data)
+
+plt.title("NCA: p1 vs p2")
+
+
+X_train_nca, X_test_nca, Y_train_nca, Y_test_nca = train_test_split(X_reduced_nca, y, test_size = test_size, random_state = 42)
+
+grid_nca = KNN_Best_Params(X_train_nca, X_test_nca, Y_train_nca, Y_test_nca)
+h = .2 # step size in the mesh
+X = X_reduced_nca
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                     np.arange(y_min, y_max, h))
+Z = grid_nca.predict(np.c_[xx.ravel(), yy.ravel()])
+
+Z = Z.reshape(xx.shape)
+plt.figure(figsize=(10,7))
+plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+# Plot also the training points
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+            edgecolor='k', s=20)
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.title("%i-Class classification (k = %i, weights = '%s')"
+          % (len(np.unique(y)),grid_nca.best_estimator_.n_neighbors, grid_nca.best_estimator_.weights))
+
+# let's find the wrong classifications we made.
+knn = KNeighborsClassifier(**grid_nca.best_params_)
+knn.fit(X_train_nca,Y_train_nca)
+y_pred_nca = knn.predict(X_test_nca)
+acc_test_nca = accuracy_score(y_pred_nca,Y_test_nca)
+print("Score --> {}".format(knn.score(X_test_nca,Y_test_nca)))
+
+test_data = pd.DataFrame()
+test_data["X_test_nca_p1"] = X_test_nca[:,0]
+test_data["X_test_nca_p2"] = X_test_nca[:,1]
+test_data["y_pred_nca"] = y_pred_nca
+test_data["Y_test_nca"] = Y_test_nca
+
+
+plt.figure(figsize=(10,7))
+diff = np.where(y_pred_nca!=Y_test_nca)[0]
+plt.scatter(test_data.iloc[diff,0],test_data.iloc[diff,1],label = "Wrong Classified",alpha = 0.2,color = "red",s = 1000)
+
+sns.scatterplot(x="X_test_nca_p1", y="X_test_nca_p2", hue="Y_test_nca",data=test_data)
+
+
+
+
+
